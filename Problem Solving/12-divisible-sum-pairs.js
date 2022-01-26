c85d56f5e15e99f73ce5d2c9514d1a12fb283396
rahul-rautwar <rahulrautwar@gmail.com>
2022-01-26 09:19:09 +0530

    add divisibleSumPairs function

---
+++ Problem Solving/12-divisible-sum-pairs.js
@@ -0,0 +1,21 @@
+/*
+ * Complete the 'divisibleSumPairs' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER k
+ *  3. INTEGER_ARRAY ar
+ */
+function divisibleSumPairs(n, k, ar) {
+    let pairCount = 0;
+    for (let i = 0; i < n; i++) {
+        for (let j = i + 1; j < n; j++) {
+            let a = (ar[i] + ar[j]) % k
+            if (a === 0) {
+                pairCount++;
+            }
+        }
+    }
+    return pairCount;
+}
