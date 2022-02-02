9a3cd5a712c3e309db4d6aaac7c9f60bcf1eb26d
rahul-rautwar <rahulrautwar@gmail.com>
2022-02-02 21:28:24 +0530

    add countingSort function

---
+++ Problem Solving/19-counting-sort.js
@@ -0,0 +1,9 @@
+/*
+ * Complete the 'countingSort' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+function countingSort(arr) {
+    return arr.sort((a, b) => a - b)
+}
